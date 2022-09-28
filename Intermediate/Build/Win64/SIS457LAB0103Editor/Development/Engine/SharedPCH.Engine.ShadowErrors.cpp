330f3c53bbe89e32558893420c900cef5b541d52
Adalid <Adalid@PORATILADI>
2022-09-28 18:47:21 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/SIS457LAB0103Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "C:/Users/Adalid/Documents/Unreal Projects/SIS457LAB0103/Intermediate/Build/Win64/SIS457LAB0103Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/SIS457LAB0103/SIS457LAB0103GameModeBase.cpp
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "SIS457LAB0103GameModeBase.h"
+
+void ASIS457LAB0103GameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// We started to generate the biography
+	GEngine->AddOnScreenDebugMessage(-1, 13.f, FColor::Red, TEXT("     BIOGRAFIA "));
+	GEngine->AddOnScreenDebugMessage(1, 14.f, FColor::Red, TEXT(" Adalid Flores Gomez "));
+	GEngine->AddOnScreenDebugMessage(2, 15.f, FColor::Black, TEXT(" Mi nombre es Adalid Flores Gomez, tengo 19 años, "));
+	GEngine->AddOnScreenDebugMessage(3, 16.f, FColor::Black, TEXT(" naci el 10 de Enero de 2003 en la ciudad de Potosi "));
+	GEngine->AddOnScreenDebugMessage(4, 17.f, FColor::Black, TEXT(" Vivo con mis padres Mayra Gomez , Braulio Flores "));
+	GEngine->AddOnScreenDebugMessage(5, 18.f, FColor::Black, TEXT(" y mi hermana Valeria de 11 años. "));
+	GEngine->AddOnScreenDebugMessage(6, 19.f, FColor::Black, TEXT(" De religion catolica, cumplí los 3 primeros "));
+	GEngine->AddOnScreenDebugMessage(7, 20.f, FColor::Black, TEXT(" sacramentos, actualmente vivo en la Ciudad de "));
+	GEngine->AddOnScreenDebugMessage(8, 21.f, FColor::Black, TEXT(" Jaime Mendoza de la ciudad de Potosi, el nivel "));
+	GEngine->AddOnScreenDebugMessage(9, 22.f, FColor::Black, TEXT(" primario lo realice en la escuela Don Bosco de la "));
+	GEngine->AddOnScreenDebugMessage(10, 23.f, FColor::Black, TEXT(" ciudad de Potosi, el nivel secundario lo realice en "));
+	GEngine->AddOnScreenDebugMessage(11, 24.f, FColor::Black, TEXT(" el colegio Nacional Junin de la ciudad de Sucre, tengo "));
+	GEngine->AddOnScreenDebugMessage(12, 25.f, FColor::Black, TEXT(" un título en técnico medio en electricidad. "));
+	GEngine->AddOnScreenDebugMessage(13, 26.f, FColor::Black, TEXT(" Estudio la carrera de Ingenieria de Sistemas, mis "));
+	GEngine->AddOnScreenDebugMessage(14, 27.f, FColor::Black, TEXT(" pasatiempos son jugar videojuegos ,ayudar  a mi "));
+	GEngine->AddOnScreenDebugMessage(15, 28.f, FColor::Black, TEXT(" familia en casa, mi color favorito es el color negro. "));
+	GEngine->AddOnScreenDebugMessage(16, 29.f, FColor::Black, TEXT(" No realizo ningun deporte debido a un  accidente "));
+	GEngine->AddOnScreenDebugMessage(17, 30.f, FColor::Black, TEXT(" que tuve hace unos años atras, donde fue afectado mi "));
+	GEngine->AddOnScreenDebugMessage(18, 31.f, FColor::Black, TEXT(" pie izquierdo, mi juego favorito es Minecraft, uno de "));
+	GEngine->AddOnScreenDebugMessage(19, 32.f, FColor::Black, TEXT(" mis sueños es poder programar mods y videjuegos "));
+
+	
+	
+
+
+
+}
